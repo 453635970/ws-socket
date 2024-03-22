48884c04c626741fc32485548431ca652f33251d
mayingkun <453635970@qq.com>
2024-03-22 16:59:12 +0800

    123
    Signed-off-by: mayingkun <453635970@qq.com>

--- ws-socket/ws-socketDlg.cpp
+++ ws-socket/ws-socketDlg.cpp
@@ -103,7 +103,14 @@ BOOL CwssocketDlg::OnInitDialog()
 	SetIcon(m_hIcon, TRUE);			// 设置大图标
 	SetIcon(m_hIcon, FALSE);		// 设置小图标
 
-	// TODO: 在此添加额外的初始化代码
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
 
 	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
 }
